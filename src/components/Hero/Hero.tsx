87ddfde2d331bd490dd3357cd0602ec093266461
Nathan Yang <nathanyang.w@outlook.com>
2026-07-01 21:06:35 +0200

    Ajoute un style temporaire pour le Hero Header

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,15 @@
 // src/components/Hero/Hero.tsx
 
+import styles from "./Hero.module.css";
+
 /** Composant react représentant la première page à présenter aux utilisateurs. */
 export default function Hero() {
   return (
-    <section id="hero">
-      <h1>Bienvenue</h1>
+    <section id="home" className={styles.heroImg}>
+      <div className={styles.heroText}>
+        <h1>Bienvenue</h1>
+        <p>Préparer vos aventures !</p>
+      </div>
     </section>
   );
 }
