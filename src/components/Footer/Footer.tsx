3002ab1e545d7104a841b800c20e2897f9adb7b6
Nathan Yang <nathanyang.w@outlook.com>
2026-07-01 20:37:12 +0200

    Ajout du pied de page

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+// src/components/Footer/Footer.tsx
+
+/** Composant react représentant le pied de la page. */
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; Nom de l'entreprise. Tous droits réservés.</p>
+    </footer>
+  );
+}
