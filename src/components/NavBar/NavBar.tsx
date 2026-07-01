959516e7be1d14195403522920e4e5ab4171277f
Nathan Yang <nathanyang.w@outlook.com>
2026-07-01 20:15:56 +0200

    Ajout du composant pour la barre de navigation

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+// src/composants/NavBar/NavBar.tsx
+
+export default function NavBar() {
+  const navigationLinks: { href: string; textContent: string }[] = [
+    { href: "#home", textContent: "Accueil" },
+    { href: "#about", textContent: "À Propos" },
+    { href: "#products", textContent: "Produits" },
+    { href: "#contact", textContent: "Contact" },
+  ];
+  return (
+    <nav>
+      {navigationLinks.map((navigationLink) => (
+        <a href={navigationLink.href} key={navigationLink.textContent}>
+          {navigationLink.textContent}
+        </a>
+      ))}
+    </nav>
+  );
+}
