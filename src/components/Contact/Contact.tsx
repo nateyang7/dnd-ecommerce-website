d657062fd208687310ceb13c1cd7fc3f3a12eb4b
Nathan Yang <nathanyang.w@outlook.com>
2026-07-06 16:12:47 +0200

    Add structure to About component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,12 +1,61 @@
-// src/components/Contact/Contact.tsx
-
 import styles from "./Contact.module.css";
 
 /** Composant react représentant la section pour contacter l'entreprise. */
 export default function Contact() {
+  function handleSubmit(): void {
+    return;
+  }
+
   return (
     <section id="contact" className={styles.contact}>
       <h2>Nous contacter</h2>
+      <form action={handleSubmit}>
+        <div className={styles.formInput}>
+          <label htmlFor="firstName">Prénom : </label>
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            required
+            size={20}
+            placeholder="Votre prénom..."
+          ></input>
+        </div>
+
+        <div className={styles.formInput}>
+          <label htmlFor="lastName">Nom</label>
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            required
+            size={20}
+            placeholder="Votre nom..."
+          ></input>
+        </div>
+
+        <div className={styles.formInput}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            size={30}
+            placeholder="gandalf@gmail.com"
+          ></input>
+        </div>
+
+        <div className={styles.formInput}>
+          <label htmlFor="subject">Sujet</label>
+          <textarea
+            name="subject"
+            id="subject"
+            placeholder="Écrire un message..."
+          ></textarea>
+        </div>
+
+        <button type="submit">Envoyer</button>
+      </form>
     </section>
   );
 }
