90fb60704dbdcc57edc9c1060ab819299c1f5564
Nathan Yang <nathanyang.w@outlook.com>
2026-07-07 16:34:24 +0200

    Finish About layout on mobile

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,15 +6,22 @@ export default function About() {
   return (
     <section id="about" className={styles.about}>
       <h2>À Propos</h2>
-      <p>
-        Notre équipe formé de passionnés pour les jeux de rôles sur tables.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
-      <img className={styles.teamPhoto} src={teamImg} alt="Photo de l'équipe" />
+      <div className={styles.teamInfo}>
+        <p>
+          Notre équipe formé de passionnés pour les jeux de rôles sur tables.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <img
+          className={styles.teamPhoto}
+          src={teamImg}
+          alt="Photo de l'équipe"
+        />
+      </div>
     </section>
   );
 }
